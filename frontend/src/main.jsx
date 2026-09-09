f97fed66948f44c0aee9fe051239e0d55e0ac638
DonRiko <l16020510@veracruz.tecnm.mx>
2026-09-08 18:00:51 -0600

    feat: envuelve la aplicación con el ThemeProvider
    Coloca el ThemeProvider en la raíz para que el tema esté disponible
    en toda la aplicación mediante useContext.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeProvider.jsx'
 import { ChatProvider } from './context/ChatProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChatProvider>
-      <App />
-    </ChatProvider>
+    <ThemeProvider>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
